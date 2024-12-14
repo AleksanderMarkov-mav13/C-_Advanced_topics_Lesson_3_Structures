ea3e19c31f57238a8656063255d490d37df8c5c8
Alexander Markov <markov_av_77@mail.ru>
2024-12-14 14:53:27 +0400

    3_1 block list

--- 3_1_vedomost_struct.cpp
+++ 3_1_vedomost_struct.cpp
@@ -3,16 +3,15 @@
 #include <fstream>
 #include <vector>
 using namespace std;
-struct vedomost
-{
-    string name = "unknown";
+struct vedomost {
     string surname = "unknown";
+    string name = "unknown";
     string date = "DD.MM.YYYY";
     int sum = 0;
 };
 int main()
 {
-    vector <vedomost> vedomost;
+    vector <vedomost> vec_vedomost;
     cout << "\033[2J\033[1;1H";
     cout << "This programme for counting salary sum we pay our workers "<< endl;
     cout << "Input your action: LIST if you want to see the list of payments" << endl;
@@ -21,7 +20,15 @@ int main()
     while (choice != "list" && choice != "add") {
         cin >> choice;
         if (choice == "list") {
-            cout << "list";
+            ifstream file("vedomost_1.txt");
+            vedomost temp;
+            while (file >> temp.surname, file >> temp.name, file >> temp.date, file >> temp.sum) {
+                vec_vedomost.push_back(temp);
+            }
+            // Вывод на экран массива:
+            for (const auto &v : vec_vedomost) {
+                cout << v.surname << " " << v.name << " " << v.date << "  " << v.sum << endl;
+            }
             break;
         }
         else if (choice == "add"){
@@ -29,7 +36,8 @@ int main()
             break;
         }
         else {
-            cout << "Incorrect input! Please repeat...";
+            cout << "Incorrect input! Please repeat..." << endl;
         }
     }
+    
 }
