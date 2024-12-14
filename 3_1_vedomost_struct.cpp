18b8062814c26dceb298af50299cdb5643b8a034
Alexander Markov <markov_av_77@mail.ru>
2024-12-14 15:40:04 +0400

    3_1 Task completed

--- 3_1_vedomost_struct.cpp
+++ 3_1_vedomost_struct.cpp
@@ -14,27 +14,23 @@ int main()
     vector <vedomost> vec_vedomost;
     cout << "\033[2J\033[1;1H";
     cout << "This programme for counting salary sum we pay our workers "<< endl;
-    cout << "Input your action: LIST if you want to see the list of payments" << endl;
-    cout << "And ADD if you want to add payment to the list" << endl;
     string choice;
-    ifstream file("vedomost_1.txt");
     vedomost temp;
-    while (file >> temp.surname, file >> temp.name, file >> temp.date, file >> temp.sum)
-    {
-        vec_vedomost.push_back(temp);
-    }
-    file.close();
-    while (choice != "list" && choice != "add") {
+    while (choice != "end") {
+        cout << "Input your action: LIST or ADD or END to finish: ";
         cin >> choice;
         if (choice == "list") {
+            ifstream file("vedomost_1.txt");
+            while (file >> temp.surname, file >> temp.name, file >> temp.date, file >> temp.sum) {
+                vec_vedomost.push_back(temp);
+            }
+            file.close();
             // Вывод на экран массива:
             for (const auto &v : vec_vedomost) {
                 cout << v.surname << " " << v.name << " " << v.date << "  " << v.sum << endl;
             }
-            break;
         }
         else if (choice == "add"){
-            // string surname, name, date, sum;
             cout << "Input surname: ";
             cin >> temp.surname;
             cout << "Input name: ";
@@ -45,12 +41,10 @@ int main()
             cin >> temp.sum;
             vec_vedomost.push_back(temp);
             ofstream file("vedomost_1.txt", ios::app);
-            file << temp.surname << " " << temp.name << " " << temp.date << " " << temp.sum << endl;
-            // for (const auto &v : vec_vedomost) {
-            //     file >> v.surname >> " " >> v.name >> " " >> v.date >> "  " >> v.sum >> endl;
-            // }
-            break;
+            file << endl << temp.surname << " " << temp.name << " " << temp.date << " " << temp.sum;
+            file.close();
         }
+        else if (choice == "end") break;
         else {
             cout << "Incorrect input! Please repeat..." << endl;
         }
